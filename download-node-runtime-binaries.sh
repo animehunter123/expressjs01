b8ccec82eb2a0f857af823b4b7e85fcfab98feb7
kenshin <kenshin@barret.local>
2024-12-03 06:29:16 +0900

    ExtractToCurDir

--- download-node-runtime-binaries.sh
+++ download-node-runtime-binaries.sh
@@ -10,13 +10,13 @@ cd "$target_dir"
 # if arm, or x64 download the correct binary
 if [ "$(uname -m)" == "arm64" ]; then
     curl -L https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-arm64.tar.xz -o node-v22.11.0-linux-arm64.tar.xz
-    tar xvfJ node-v22.11.0-linux-arm64.tar.xz
+    tar xvfJ node-v22.11.0-linux-arm64.tar.xz -C .
 else
     curl -L https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-x64.tar.gz -o node-v22.11.0-linux-x64.tar.gz
-    tar xvf node-v22.11.0-linux-x64.tar.gz
+    tar xvf node-v22.11.0-linux-x64.tar.gz -C .
 fi
 
-cd node*
+# cd node*
 cd bin
 
 node_binary=$(pwd)/node
