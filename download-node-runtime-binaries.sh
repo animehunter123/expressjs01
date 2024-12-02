d2a6576c13db6d6570ab8795a1b67086e62b6720
kenshin <kenshin@barret.local>
2024-12-03 04:36:43 +0900

    fixedTarTypo

--- download-node-runtime-binaries.sh
+++ download-node-runtime-binaries.sh
@@ -10,11 +10,12 @@ cd "$target_dir"
 # if arm, or x64 download the correct binary
 if [ "$(uname -m)" == "arm64" ]; then
     curl -L https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-arm64.tar.xz -o node-v22.11.0-linux-arm64.tar.xz
+    tar xvfJ node-v22.11.0-linux-arm64.tar.xz
 else
     curl -L https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-x64.tar.gz -o node-v22.11.0-linux-x64.tar.gz
+    tar xvf node-v22.11.0-linux-x64.tar.gz
 fi
 
-tar xvfpJ *.tar.*z
 cd bin
 
 node_binary=$(pwd)/node
