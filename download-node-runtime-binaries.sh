f8f4c687f9f0199b40e02d4a77e168f120caa8d7
kenshin <kenshin@barret.local>
2024-12-03 06:34:33 +0900

    fixedaarch64silicon

--- download-node-runtime-binaries.sh
+++ download-node-runtime-binaries.sh
@@ -8,7 +8,8 @@ mkdir -p "$target_dir"
 cd "$target_dir"
 
 # if arm, or x64 download the correct binary
-if [ "$(uname -m)" == "arm64" ]; then
+# if [ "$(uname -m)" == "arm64" ]; then
+if [ "$(uname -m)" == "aarch64" ]; then
     curl -L https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-arm64.tar.xz -o node-v22.11.0-linux-arm64.tar.xz
     tar xvfJ node-v22.11.0-linux-arm64.tar.xz -C .
     cd node-v22.11.0-linux-arm64
